9f129e55fb693cdb2293bee1587a96956f98f768
kkimsuhyeon <wzxy2002@naver.com>
2021-08-12 00:37:52 +0900

    infinite-scroll

--- infinite-scroll/src/index.js
+++ infinite-scroll/src/index.js
@@ -1,20 +1,31 @@
-import './index.css';
-import renderList from './listRenderer';
+import "./index.css";
+import renderList from "./listRenderer";
+import { debounce } from "./util";
 
-const app = document.querySelector('#app');
-const fetchMoreTrigger = document.querySelector('#fetchMore');
+const app = document.querySelector("#app");
+const fetchMoreTrigger = document.querySelector("#fetchMore");
 let page = 0;
 
 const loadMore = async () => {
-	const target = page ? fetchMoreTrigger : app;
-	target.classList.add('loading');
-	await renderList(page++);
-	target.classList.remove('loading');
+  const target = page ? fetchMoreTrigger : app;
+  target.classList.add("loading");
+  await renderList(page++);
+  target.classList.remove("loading");
 };
 
-const onScroll = (e) => {
-	// do something (hint: e.target.scrollingElement)
-};
+// const onScroll = (e) => {
+//   const { clientHeight, scrollTop, scrollHeight } = e.target.scrollingElement;
+//   if (scrollTop + clientHeight === scrollHeight) {
+//     loadMore();
+//   }
+// };
 
-document.addEventListener('scroll', onScroll);
+// document.addEventListener("scroll", debounce(onScroll, 300));
+
+// const fetchMoreObserver = new IntersectionObserver((entries, observer) => {
+//   // fetchMoreTrigger의 margin-top, display style 수정해야함
+//   if (entries[0].isIntersecting) loadMore();
+// });
+// fetchMoreObserver.observe(fetchMoreTrigger);
+
 loadMore();
--- infinite-scroll/src/util.js
+++ infinite-scroll/src/util.js
@@ -1,9 +1,19 @@
 const getRandomSeconds = () => (Math.round(Math.random() * 5) + 1) * 250;
 
 export const randomTimer =
-	(func, ...args) =>
-	(resolve) => {
-		setTimeout(() => resolve(func(...args)), getRandomSeconds());
-	};
+  (func, ...args) =>
+  (resolve) => {
+    setTimeout(() => resolve(func(...args)), getRandomSeconds());
+  };
 
-export const dummyFetcher = (method, args) => new Promise(randomTimer(method, args));
+export const dummyFetcher = (method, args) =>
+  new Promise(randomTimer(method, args));
+
+export const debounce = (func, delay) => {
+  let timeoutId = null;
+
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(func.bind(null, ...args), delay);
+  };
+};
